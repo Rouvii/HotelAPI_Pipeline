13326e9c29cd5647f10269f8fefb47ed6b01dc5d
Rouvii <Danielrouvillain@gmail.com>
2024-10-08 10:59:49 +0200

    pipeline and test

---
+++ src/test/java/dk/lyngby/dao/HotelDAOTest.java
@@ -0,0 +1,33 @@
+package dk.lyngby.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
